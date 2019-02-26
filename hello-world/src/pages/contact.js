6c84c58ce5f9b77d3eb685950725db4d6877a12d
wtharris89 <wtharris89@gmail.com>
2019-02-26 15:17:59 -0700

    added contact page

--- hello-world/src/pages/contact.js
+++ hello-world/src/pages/contact.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
-    <Header headerText="It's pretty cool!" />
-    <p>Such wow. Very React.</p>
+    <Link to="/">Home</Link>
+    <Header headerText="Contact" />
+    <p>Send us a message!</p>
   </div>
 )
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div>
+    <Link to="/contact/">Contact</Link>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
   </div>

8ce1b76f15eadb85345715eeee8484f2dfb44614
wtharris89 <wtharris89@gmail.com>
2019-02-27 16:28:41 -0700

    added about.js and contact.js

---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:will.t.harris@protonmail.com">
+        will.t.harris@protonmail.com
+      </a>
+    </p>
+  </div>
+)

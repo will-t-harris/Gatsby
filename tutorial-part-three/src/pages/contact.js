dcda49a06210e4d06372b5900b9e3b27854d80db
wtharris89 <wtharris89@gmail.com>
2019-02-27 16:35:47 -0700

    added Layout to contact.js and layout.js

--- tutorial-part-three/src/pages/contact.js
+++ tutorial-part-three/src/pages/contact.js
@@ -1,12 +1,15 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-  <div>
-    <h1>I'd love to talk! Email me at the address below</h1>
-    <p>
-      <a href="mailto:will.t.harris@protonmail.com">
-        will.t.harris@protonmail.com
-      </a>
-    </p>
-  </div>
+  <Layout>
+    <div>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:will.t.harris@protonmail.com">
+          will.t.harris@protonmail.com
+        </a>
+      </p>
+    </div>
+  </Layout>
 )

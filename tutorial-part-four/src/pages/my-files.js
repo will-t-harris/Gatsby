d42082124703d2bbf00c5bd9ed4daaf9596343c9
wtharris89 <wtharris89@gmail.com>
2019-02-28 12:08:08 -0700

    created my-files.js with the allFile graphql query

---
+++ tutorial-part-four/src/pages/my-files.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <div>Hello World</div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
